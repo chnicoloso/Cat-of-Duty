b484caab188a563f6a3bf6af513e9bb91640e04c
RjLi13 <ruijing.li@berkeley.edu>
2015-01-28 23:10:55 -0800

    Trying to commit all

---
+++ Unity/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	
+	int timer;
+	public GUIText seconds;
+	// Use this for initialization
+	void Start() 
+	{
+		seconds = GetComponent<GUIText>();
+	 	timer = 120;
+		StartCoroutine(WaitAndPrint(1f));
+	
+	}
+	
+	// Update is called once per frame
+	/*void Update () 
+	{
+
+
+
+
+	}
+*/
+
+	IEnumerator WaitAndPrint(float waitTime) 
+	{
+		while (timer > 0)
+		{
+		yield return new WaitForSeconds(waitTime);
+		print("WaitAndPrint " + Time.time);
+		timer -= 1;
+		seconds.text = timer.ToString();
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponScript : MonoBehaviour {
+	public Transform bulletPrefab;
+	public float shootingRate = 0.25f;
+	private float shootCooldown;
+	private bool isRight = true;
+
+	// Use this for initialization
+	void Start () {
+		shootCooldown = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shootCooldown > 0)
+			shootCooldown -= Time.deltaTime;
+	}
+
+	public void Attack(){
+		//print ("In Attack method");
+		if (CanAttack ()) {
+			shootCooldown = shootingRate;
+			//print ("I can attack");
+
+			var bulletTransform = Instantiate (bulletPrefab) as Transform;
+			//print("this is bullettransform" + bulletTransform);
+			bulletTransform.position = transform.position;
+			//print ("this is bullet gameobject" + bulletTransform.gameObject);
+			ProjectileScript move = bulletTransform.gameObject.GetComponent<ProjectileScript> ();
+			//print ("this if check " +  (move != null));
+			//player
+			if (move != null){
+				//print ("I am in move");
+				if (Input.GetKeyDown(KeyCode.RightArrow))
+					isRight = true;
+				if(Input.GetKeyDown(KeyCode.LeftArrow))
+					isRight = false;
+					
+				print (isRight);
+
+				if(isRight)
+					move.direction = this.transform.right;
+				else
+					move.direction = this.transform.right * -1;
+				print (move.direction);
+			}
+		}
+	}
+
+	public bool CanAttack() {
+		return shootCooldown <= 0f;
+	}
+}
